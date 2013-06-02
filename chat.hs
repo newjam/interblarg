fa813ed5c50b4609e758b7edad966eb8986f84c1
Ubuntu <ubuntu@ip-10-202-95-119.ec2.internal>
2013-06-02 11:39:03 +0000

    hopefully fixed time out bug

--- chat.hs
+++ chat.hs
@@ -8,7 +8,10 @@ import qualified Data.Conduit.List as CL
 
 import Data.Monoid (mconcat)
 import Control.Monad.IO.Class (liftIO, MonadIO)
+import Control.Monad (forM_)
 import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as C
+
 
 import Blaze.ByteString.Builder
 import Blaze.ByteString.Builder.ByteString
@@ -19,16 +22,29 @@ import Database.Redis.Core
 
 import Network.Wai.EventSource
 
+import Control.Concurrent.Timer (repeatedTimer)
+import Control.Concurrent.Suspend (sDelay)
+
+
 main = scotty 8000 routes
 
+file' ct fn = do
+  header "Content-Type" ct
+  file fn
+
 routes = do
-  get  "/chat.css"   $ file "chat.css"
-  get  "/chat.js"    $ file "chat.js"
-  get  "/:chan"      $ file "chat.html"
-  post "/:chan"        postMessage
+  -- if we don't send data to subscribers periodically,
+  -- the http connection will tiome out.
+  conn <- liftIO $ connect defaultConnectInfo
+  liftIO $  repeatedTimer (keepAlive conn) (sDelay 10)
+
+  get  "/channels"     listChannels
   get  "/:chan/stream" messageStream
   get  "/:chan/recent" recentMessages
-
+  get  "/chat.css"   $ file' "text/css" "chat.css"
+  get  "/chat.js"    $ file' "text/javascript" "chat.js"
+  get  "/:chan"      $ file' "text/html" "chat.html"
+  post "/:chan"        postMessage
 
 postMessage chan = do
   conn <- liftIO . connect $ defaultConnectInfo
@@ -36,6 +52,7 @@ postMessage chan = do
   let msg' = toByteString . fromLazyByteString $ msg
   liftIO . runRedis conn $ do
     -- add exception handling
+    R.sadd "channels" [chan]
     R.lpush chan [msg']
     R.ltrim chan 0 9
     R.publish chan msg'
@@ -54,9 +71,21 @@ recentMessages chan = do
   conn <- liftIO . connect $ defaultConnectInfo
   messages <- liftIO . runRedis conn $ do
     results <- R.lrange chan 0 9
-    let toMaybe = either (const Nothing) Just
     return . toMaybe $ results
   json messages
+
+
+toMaybe = either (const Nothing) Just
+
+listChannels = do
+  conn <- liftIO $ connect defaultConnectInfo
+  channels <- liftIO . runRedis conn $ R.smembers "channels"
+  json . toMaybe $ channels
+
+keepAlive conn = runRedis conn $ do
+  Right chans <- R.smembers "channels"
+  forM_ chans $ \chan ->
+    publish chan ":keep alive"
 
 -- Redis Stuff
 subscribe' :: B.ByteString -> Redis (Either R.Reply B.ByteString)
@@ -72,9 +101,10 @@ sourceRedisChannel conn chan = do
   let loop = recvMsg conn >> loop
   loop
 
-pubsubToBS (Msg (Message _ msg)) =
-  fromByteString msg
+pubsubToBS (Msg (Message _ msg)) = msg
 
-redis2EventSource = CL.map $ 
-  ServerEvent Nothing Nothing . return . pubsubToBS
+redis2EventSource = CL.map $ quz . pubsubToBS
 
+quz msg = if C.head msg == ':'
+  then CommentEvent . fromByteString . C.tail $ msg
+  else ServerEvent Nothing Nothing [fromByteString msg]
