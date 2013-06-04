b32e4b78219457494cfdd91547588b685ee14122
Ubuntu <ubuntu@ip-10-202-95-119.ec2.internal>
2013-06-04 10:12:07 +0000

    whoops these files didn't get commited

--- chat.js
+++ chat.js
@@ -3,6 +3,7 @@ var baseURL = document.URL
 var stream = new EventSource(baseURL + '/stream')
 
 var imgRegex = RegExp('(http[s]?:\/\/.*\.(?:png|jpg|jpeg|gif))')
+var urlRegex = RegExp('(http[s]?:\/\/.*)')
 
 stream.onerror = function(e) {
   // try again?
@@ -14,8 +15,11 @@ stream.onmessage = function(e) { addMessage(e.data) }
 
 addMessage = function(msg) {
   obj =  $.parseJSON(msg)
-  inner = imgRegex.test(msg) ? "<img src='" + obj.payload + "'/>" : obj.payload
-
+  inner = imgRegex.test(obj.payload)
+    ? "<img src='" + obj.payload + "'/>"
+    : urlRegex.test(obj.payload)
+      ? "<a href='" + obj.payload + "'>" + obj.payload + "</a>"
+      : obj.payload
   $('#inputDiv').after("<div class='" + obj.colorId + "'>" + inner + "</div>")
 }
 
