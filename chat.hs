197304b1fa352464dfc3f1e68095cf183db621df
Ubuntu <ubuntu@ip-10-202-95-119.ec2.internal>
2013-07-14 07:25:19 +0000

    fiddled a bunch didn't commit :/

--- chat.hs
+++ chat.hs
@@ -27,6 +27,7 @@ import qualified Database.Redis as R
 import Database.Redis.PubSub
 import Database.Redis.Core
 
+import Network.Wai.EventSource.EventStream
 import Network.Wai.EventSource
 import Network.Wai
 
@@ -37,24 +38,49 @@ import Crypto.Hash (hash, digestToByteString, SHA1, SHA512, Digest)
 
 import Data.String
 
-import Data.ByteString.Base64 as B64
+import Data.ByteString.Base64.URL as B64
 
 import Network.HTTP.Types.Header (hUserAgent)
 
 import qualified Network.Socket as S
 
 import qualified Data.Aeson.Generic as JSON
+import Data.Aeson (ToJSON(..), (.=), object)
 
 import Data.Semigroup
+import Control.Applicative
+
+import Data.Time.Clock.POSIX (getPOSIXTime)
+import Data.Time.Clock
+{- newtype MyRedis a = Redis (Either Reply a)
+
+get :: RedisCtx
+
+instance MonadRedis MyRedis where
+  liftRedis :: 
+
+I want something like
+
+add :: ByteString -> ByteString ->  MyRedis (Maybe Int)
+add k1 k2 = do
+  ma <- R.get k1 
+  mb <- R.get k2
+  return $ (+) <$> ma <*> mb
+  
+
+-}
+
 
 instance Semigroup B.ByteString where
   (<>) = mappend
 
 main = scotty 8000 routes
 
-data Blah = Blah
+data ChatMsg = ChatMsg
   { payload :: B.ByteString
-  , colorId :: B.ByteString
+--  , colorId :: B.ByteString -- Should probable have better type, not just BS
+  , user :: B.ByteString
+  , time :: Integer
   } deriving (Data, Typeable, Show)
 
 
@@ -69,14 +95,19 @@ routes = do
   conn <- liftIO $ connect defaultConnectInfo
   liftIO $  repeatedTimer (keepAlive conn) (sDelay 10)
 
-  get  "/identify"   $ request >>= json . identify
-  get  "/channels"     listChannels
-  get  "/:chan/stream" messageStream
-  get  "/:chan/recent" recentMessages
-  get  "/chat.css"   $ file' "text/css" "chat.css"
-  get  "/chat.js"    $ file' "text/javascript" "chat.js"
-  get  "/:chan"      $ file' "text/html" "chat.html"
-  post "/:chan"        postMessage
+  get  "/identify"     $ request >>= json . identify
+  get  "/channels"       listChannels
+  get  "/:chan/stream"   messageStream
+  get  "/:chan/recent"   recentMessages
+  get  "/:chan/me"       getMe
+  post "/:chan/me"       changeMe
+  get  "/:chan/:poster"  userInfo
+  get  "/chat.css"     $ file' "text/css" "chat.css"
+  get  "/chat.js"      $ file' "text/javascript" "chat.js"
+  get  "/:chan"        $ file' "text/html" "chat.html"
+  post "/:chan"          postMessage
+
+
 
 
 fuck r = r >>= either 
@@ -87,69 +118,309 @@ fucking r = r >>= maybe
   (error "you thought you didn't fuck up but you did.")
   return
 
-postMessage chan = do
+{--
+ 
+ on post, parse input and validate it so we don't publish funky or maliscious
+ messages such as <script>alert("loser!")</script>
+
+ also: refactor. this is ugly.
+
+ timestamp messages
+
+ allow for multiple streams at once, ie /stream1+stream2 would merge
+ the two, sorting by timestamp, posting would publish to both.
+ How to resolve the colors?
+
+ be able to upload images to amazon cdn
+
+ ooooh! refactor, make a function getOrAssignColor
+ and instead of saving the color with the msg, just
+ save the user. when loading messages on client
+ get the color of the user, and color the post that.
+
+ IMPORTANT: This leaves orphan subscriptions to redis.
+ If this were real, that would be pretty important to fix.
+ if the http connection is terminated, everything else should too.
+
+--}
+
+{-
+
+input: chan, client
+
+add poster to chan's set of clients
+if client hasn't posted in chan before, assign them a new id, the card of the 
+associate that client with that id
+
+-}
+
+foobar chan poster = do
+  let chan' = "chan[" <> chan <> "]"
+  let posters' = (chan' <> ".posters")
+  let poster' = posters' <> "[" <> poster <> "]" -- posters[" <> poster <> "]"
+  i <- fuck $ R.sadd posters' [poster]
+  if i == 1
+  then do
+    -- get the number associated with this poster
+    fucking . fuck $ R.get poster'
+  else do-- add
+    n <- fuck $ R.scard posters'
+    let n' = fromString . show $ n 
+    void $ R.set poster' n'
+    return n'
+
+getOrAssignColor chan poster = do
+  let posters = "chan["<>chan<>"].users" -- <> "-posters"
+  -- if this is a new user assign them a number
+  numRemoved <- fuck $ R.lrem posters 1 poster
+  numPosters <- fuck $ R.lpush posters [poster]
+
+  let toKey p = "chan["<>chan<>"].users["<>p<>"].color" --   chan <> "-" <> p
+
+  let numColors = 16 
+  i <- if numPosters > numColors
+  then do
+    oldPoster <- fucking . fuck $ R.rpop posters
+    oldNumber <- fucking . fuck $ R.get (toKey oldPoster)
+    R.del [oldPoster]
+    return oldNumber 
+    --liftIO . print $ "end of cycle, last active poster: " <> oldPoster
+  else
+    return . fromString . show $ numPosters
+
+  let isNewPoster = numRemoved == 0
+  if isNewPoster 
+  then do
+    void $ R.set (toKey poster) i
+    return i
+  else fucking . fuck $ R.get (toKey poster)
+
+--getName chan poster color = return $ "user" <> color
+  
+lazy2Strict = toByteString . fromLazyByteString
+
+data User = User {
+    userId :: C.ByteString
+  , userColor :: Maybe C.ByteString
+  , userName :: Maybe C.ByteString
+}
+
+instance ToJSON User where
+  toJSON (User id color name) = object ["id" .= id, "color" .= color, "name" .= name]
+
+changeMe :: C.ByteString -> ActionM ()
+changeMe chan = do
+  user <- identify <$> request
+  let nameKey = "chan["<>chan<>"].users["<>user<>"].name"
+  newName <- lazy2Strict <$> body
   conn <- liftIO . connect $ defaultConnectInfo
-  req <- request
-  let poster = identify req
-  msg <- body
-  let lazy2Strict = toByteString . fromLazyByteString
-  let msg' = lazy2Strict msg
-  liftIO . runRedis conn $ do
-    -- add exception handling
-    R.sadd "channels" [chan]
-    let posters = chan <> "-posters"
-    -- if this is a new user assign them a number
-    numRemoved <- fuck $ R.lrem posters 1 poster
-    n <- fuck $ R.lpush posters [poster]
+  liftIO $ runRedis conn $ do
+    R.set nameKey newName
 
-    let numColors = 16 
-    i <- if n > numColors
-    then do
-      oldPoster <- fucking . fuck $ R.rpop posters
-      oldNumber <- fucking . fuck $ R.get (chan <> "-" <> oldPoster)
-      R.del [oldPoster]
-      return oldNumber 
-      --liftIO . print $ "end of cycle, last active poster: " <> oldPoster
-    else
-      return . fromString . show $ n
+    --let event = serverEvent "updateUser" msg
 
-    let isNewPoster = numRemoved == 0
-    x <- if isNewPoster 
-    then do
-      void $ R.set (chan <> "-" <> poster) i
-      return i
-    else fucking . fuck $ R.get (chan <> "-" <> poster)
-
-    let msg'' = lazy2Strict . JSON.encode $ Blah msg' ("color" <> x)
-
-    R.lpush chan [msg'']
-    R.ltrim chan 0 9
-    R.publish chan msg''
+    -- publish msg to all clients
+    --R.publish chan event
   text "ok"
+
+  --body >>= liftIO . print
+  --body >>= json
+
+-- 
+
+-- createUser :: ByteString -> Redis User
+-- set default user info, eg {name:"user1", color:"1"}
+
+-- getUser :: '''
+-- get user info. if the info doesn't exist, createUser
+
+
+--createUser :: C.ByteString -> C.ByteString -> Redis User
+--createUser chan userId = do
+  -- color
+  -- name
+  -- user
+  --return $ User userId color name
+
+  -- if userId is in the set of users
+  -- then 
+
+
+{-
+have a color pool. ask for a color, if there is a color in pool, give one
+otherwise, take a color back from a user, and return it. make sure
+to delete the key value pair for that user/color association.
+-}
+
+createName :: C.ByteString -> C.ByteString -> Redis C.ByteString
+createName chan userId = do
+  let base = "chan["<>chan<>"]"
+  let counterKey = base<>".nameCounter"
+  x <- fuck $ R.incr counterKey
+  let nameKey = base<>".users["<>userId<>"].name"
+  let name = "user"<>(fromString . show $ x)
+  R.set nameKey name
+  return name
+
+userInfo chan userId = do
+  conn <- liftIO . connect $ defaultConnectInfo
+  let nameKey = "chan["<>chan<>"].users["<>userId<>"].name"
+  info <- liftIO . runRedis conn $ do
+    color <- getOrAssignColor chan userId
+
+--    name <- maybe setName return . fuck . R.get $ nameKey
+ 
+    mName <- fuck . R.get $ nameKey 
+    name <- maybe (createName chan userId) (return) mName
+    
+    {-name <- case mName of
+      Just name -> return $ Just name 
+      Nothing   -> do
+        let name = "user"<>color
+        R.set nameKey name
+        return $ Just name
+           --name <- maybe ("user"<>color) id <$> (fuck . R.get $ nameKey);
+    -}
+    return $ User userId (Just color) (Just name)
+  
+  json info
+{-
+  let getColor = getOrAssignColor chan poster 
+  let nameKey = "chan["<>chan<>"]users["<>poster<>"].name"
+  let getName color = maybe ("user"<>x) id <$> (fuck . R.get $ nameKey)
+  color <- liftIO . runRedis conn $ stuff >>= thing
+  
+  --json $ User "" "" ""
+  json $ User poster color ("user" <> color)
+-}
+getMe = identify <$> request >>= json
+
+
+
+postMessage chan = do
+  conn    <- liftIO . connect $ defaultConnectInfo
+  poster  <- identify    <$> request
+  payload <- lazy2Strict <$> body
+  time    <- truncate  <$> liftIO getPOSIXTime
+  listeners <- liftIO . runRedis conn $ do
+    -- add this channel to the set of all channels
+    -- [not used yet, but interesting]
+    R.sadd "channels" [chan]
+
+    let encode = lazy2Strict . JSON.encode
+    let msg = encode $ ChatMsg payload poster time
+
+    -- add msg to persistent list of msgs
+    R.lpush chan [msg]
+    -- keep only 100 msgs
+    R.ltrim chan 0 99
+
+    -- translate message into valid text/event-source syntax
+    let event = serverEvent "newMessage" msg
+
+    -- publish msg to all clients
+    fuck $ R.publish chan event
+  debug $ "postMessage " <> (C.pack . show $ listeners)
+  text "ok"
+
+debug x = liftIO . putStrLn . C.unpack $ x
+
+
+{-
+
+get's a users info, for now just the color, heh.
+
+-}
+
+--user chan id = fucking . fuck $ R.get ("chan[" <> chan <> ".users[" <> id <> "]")
+  
+
+{-
+
+>>>> data model <<<<
+
+have a client side function that turns a user # into 
+a color. Have it always return the same color for each client
+Decouple data from presentation. except the user can't change the color :(
+
+post
+  payload
+  user
+
+this is a goal, not the current reality
+
+list of messages in channel
+
+  endpoint
+    /test/messages
+
+  redis key
+    channels.test.messages
+
+  value
+    [8,7,6,5]
+
+single message in channel
+      
+  endpoint
+    /test/messages/8
+
+  redis key
+    channels.test.messages.8
+
+  value
+    {
+      payload:"Hello, world.",
+      user:"5ag5fcsw6jgfh90g3df"
+    }
+
+single user from channel
+
+  endpoint
+    /test/users/5ag5fcsw6jgfh90g3df
+
+  redis key
+    channels.test.users.5ag5fcsw6jgfh90g3df
+
+  value
+    {
+      color:0,
+      name:"James"
+    }
+
+-}
 
 messageStream chan = do
   -- act as an EventSource stream
   header "Content-Type" "text/event-stream"
   -- create a connection to Redis
   conn <- liftIO . connect $ defaultConnectInfo
-  -- create a conduit Source
-  let stream = sourceRedisChannel conn chan
-  source . sourceToSource $ (stream $= redis2EventSource) 
+  -- Source of Bytestring from listening to a Redis pubsub channel
+  let stream  = sourceRedisChannel conn chan
+  -- turn Source of ByteString into Source of  Flush Builder
+  let builder = CL.concatMap $ \x -> [Chunk $ insertByteString x, Flush]
+  
+  debug "subscribed"
+
+  source $ stream $= builder 
 
 recentMessages chan = do 
   conn <- liftIO . connect $ defaultConnectInfo
   messages <- liftIO . runRedis conn $ do
-    results <- R.lrange chan 0 9
+    results <- R.lrange chan 0 99
     return . toMaybe $ results
   json messages
 
-foob :: S.SockAddr -> B.ByteString
-foob (S.SockAddrInet _  addr) = fromString . show $ addr
-foob (S.SockAddrInet6 _ _ (a, b, c, d) _) = B.concat . map (fromString . show) $ [a,b,c,d]
-foob (S.SockAddrUnix sock) = fromString sock
+foo :: ServerEvent -> C.ByteString
+foo =  maybe "" toByteString . eventToBuilder
+
+addr :: S.SockAddr -> B.ByteString
+addr (S.SockAddrInet _  addr) = fromString . show $ addr
+addr (S.SockAddrInet6 _ _ (a, b, c, d) _) = B.concat . map (fromString . show) $ [a,b,c,d]
+addr (S.SockAddrUnix sock) = fromString sock
 
 identify req = B64.encode . sha1. B.concat $ [user_agent, ip] where
-    ip = fromString . show . foob . remoteHost $ req
+    ip = fromString . show . addr . remoteHost $ req
     user_agent = maybe "" id . lookup hUserAgent .
         requestHeaders $ req
 
@@ -163,16 +434,16 @@ listChannels = do
 keepAlive conn = runRedis conn $ do
   Right chans <- R.smembers "channels"
   forM_ chans $ \chan ->
-    publish chan ":keep alive"
+    publish chan . serverComment $ "keep alive"
 
 -- Redis Stuff
 subscribe' :: B.ByteString -> Redis (Either R.Reply B.ByteString)
 subscribe' channel = sendRequest ["SUBSCRIBE", channel]
 
-recvMsg :: MonadIO m => Connection -> Source m PubSubReply
 recvMsg conn = do
   r <- liftIO . runRedis conn $ recv
-  yield . decodeMsg $ r
+  yield . pubsubToBS . decodeMsg $ r
+
 
 sourceRedisChannel conn chan = do
   liftIO . runRedis conn . subscribe' $ chan
@@ -181,11 +452,10 @@ sourceRedisChannel conn chan = do
 
 pubsubToBS (Msg (Message _ msg)) = msg
 
-redis2EventSource = CL.map $ quz . pubsubToBS
-
-quz msg = if C.head msg == ':'
-  then CommentEvent . fromByteString . C.tail $ msg
-  else ServerEvent Nothing Nothing [fromByteString msg]
+serverEvent n d = foo $ ServerEvent n' Nothing [d'] where
+  n' = Just . fromByteString . C.cons ' ' $ n
+  d' = fromByteString d
+serverComment   = foo . CommentEvent . fromByteString 
 
 -- Crypto
 sha1 :: B.ByteString -> B.ByteString
--- chat.js
+++ chat.js
@@ -2,8 +2,8 @@
 var baseURL = document.URL
 var stream = new EventSource(baseURL + '/stream')
 
-var imgRegex = RegExp('(http[s]?:\/\/.*\.(?:png|jpg|jpeg|gif))')
-var urlRegex = RegExp('(http[s]?:\/\/.*)')
+var imgRegex = RegExp(/(http[s]?:\/\/.*\.(?:png|jpg|jpeg|gif))/)
+var urlRegex = RegExp(/((ftp|http|https):\/\/[^ "]+)/)
 
 stream.onerror = function(e) {
   // try again?
@@ -11,34 +11,90 @@ stream.onerror = function(e) {
   console.log('EventSource error')
 }
 
-stream.onmessage = function(e) { addMessage(e.data) }
+stream.addEventListener("newMessage", function(e){
+  addMessage(e.data)
+})
 
-addMessage = function(msg) {
-  obj =  $.parseJSON(msg)
-  inner = imgRegex.test(obj.payload)
-    ? "<img src='" + obj.payload + "'/>"
-    : urlRegex.test(obj.payload)
-      ? "<a href='" + obj.payload + "'>" + obj.payload + "</a>"
-      : obj.payload
-  $('#inputDiv').after("<div class='" + obj.colorId + "'>" + inner + "</div>")
-}
-
-Zepto(function($){
-  $('#input').on('keypress', function(e){
-    if(e.keyCode == '13') {
-      $.post(baseURL, this.value, function(r){
-        if(r === 'ok' ){
-          $('#input').val('')
-        }
-      })
+wrapUrls = function(x){
+  return x.replace(urlRegex, function(url){  
+    console.log(url)
+    if(imgRegex.test(url)){
+      return $('<img/>', {src:url}).prop('outerHTML') //"<img src='" + url + "'/>"
+    }else {
+      return $('<a/>', {href:url, text:url}).prop('outerHTML')
     }
   })
+}
 
+changeName = function() {
+  newName = $(this).val()
+
+  $.post(baseURL + '/me', newName, function(r){ console.log(r) }) 
+
+  alert(newName)
+}
+
+addMessage = function(msg) {
+  console.log(msg)
+
+  var obj =  $.parseJSON(msg)
+
+  var time = $('<div/>').addClass('time').text(moment.unix(obj.time).fromNow())
+  var name = $('<div/>').addClass('name')
+
+  var head = $('<div/>').addClass('itemHeader').append(name).append(time)
+    
+  var body = $('<div/>').html(wrapUrls(obj.payload))
+
+  var x = $('<div/>').append(head).append(body).data('user', obj.user)
+
+  $('#inputDiv').after(x)
+
+  $.getJSON(baseURL + '/' + obj.user, function(userInfo){
+    x.addClass('color' + userInfo.color)
+
+    if(userInfo.id === me){
+      name.append($('<input/>').val(userInfo.name).onEnter(changeName))
+    }else{
+      name.text(userInfo.name)
+    }
+    
+  })
+}
+
+;(function($){
+  $.extend($.fn, {
+    onEnter: function(f){
+      $(this).on('keypress', function(e){
+        if(e.keyCode == '13') {
+          f.call(this)
+        }
+      })
+      return this
+    }
+  })
+})(Zepto)
+
+Zepto(function($){
+ 
+  $.getJSON(baseURL + '/me', function(id){
+    me = id
+  })
+ 
+  $('#input').onEnter(function(){
+    $.post(baseURL, this.value, function(r){
+      if(r === 'ok' ){
+        $('#input').val('')
+      }
+    })
+  })
+ 
+
+  x = $.getJSON(baseURL + '/recent')
 
   // load the previous messages
   $.getJSON(baseURL + '/recent', function(data) {
     data.reverse()
     data.forEach(addMessage)
   })
-
 })
